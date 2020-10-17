b23665126ae09d8de7aaa56ce06b82ca5db4c4fd
Yairina Boada <yairinaboada@gmail.com>
2020-10-17 01:33:02 -0300

    Exercicio 01 de JS

---
+++ Exercicio-01.js
@@ -0,0 +1,25 @@
+//Usando o prompt
+
+    let name = prompt("Digite seu nome");
+        console.log("Olá, " + name);
+
+    let lastName = prompt("Digite seu sobrenome");
+        console.log("Nome e sobrenome: " + name + " " + lastName);
+
+    let age = prompt("Digite seu idade");
+        console.log("Idade: " + age);
+
+    let country = prompt("Digite lugar de nascimento");
+        console.log("Lugar de nascimento: " + country);
+
+    let profession = prompt("Digite sua profissão");
+        console.log("Profissão: " + profession);
+
+// Extra!!!
+
+console.table({
+    Nome: "Yairina",
+    Sobrenome: "Boada",
+    Idade: "24",
+    Formação: "Full-Stack Student Developer",
+})
